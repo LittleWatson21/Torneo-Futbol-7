5c5a05f64b3d644ac938db65820a1733e839ee9d
entrenadorDeFocas <34198886+LittleWatson21@users.noreply.github.com>
2019-06-13 12:42:33 -0500

    Create Jornada.cs

---
+++ Jornada.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Parcial_3
+{
+    class Jornada
+    {
+        public int numero { get; }
+
+        List<Enfrentamiento> enfrentamientos;
+        public  Jornada(int Numero)
+        {
+            numero = Numero;
+            enfrentamientos = new List<Enfrentamiento>();
+        }
+
+        public void agregarEnfrentamiento (Equipo local, Equipo visitante)
+        {
+
+        }
+    }
+}
